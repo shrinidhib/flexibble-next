79b493eb238d94348d7def54aaea7a293a856ee8
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-04-29 10:56:51 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarkable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
